8a285faf139cd144a1e0c55b6521bb821597d8b1
myfriendscallmebode <bode.aaron.m@gmail.com>
2020-03-15 13:26:05 -0700

    tried a few more things that didn't work

--- gru-test.py
+++ gru-test.py
@@ -53,49 +53,33 @@ def tensor_from_text(lang, sentence):
 class Net(nn.Module):
     def __init__(self, vocab_size, seq_length, embedding_size, hidden_size, padding_idx):
         super(Net, self).__init__()
-        self.embedding_size = embedding_size
         self.hidden_size = hidden_size
+        self.embedding_size = embedding_size
         self.embedding = nn.Embedding(vocab_size, embedding_size, padding_idx) #learns embedding, input size is n_words, maybe experiment with different representation
         self.gru = nn.GRU(input_size = seq_length*embedding_size, 
         					hidden_size = hidden_size,
-        					num_layers=3)
-        #self.dense = nn.Linear(seq_length*embedding_size, hidden_size) #throw a connected layer in there
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model = hidden_size, nhead = 8) #d_model = hidden_size must be divisible by nhead
-        self.trans_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=6)  #If we want many encoder layers
-        self.fc1 = nn.Linear(hidden_size + 1, 50) #try different number of hidden units?
-        self.fc2 = nn.Linear(50, 3)
+        					num_layers=1) #trying different nuymber of layers
+        self.fc1 = nn.Linear(hidden_size + 1, 20) #try different number of hidden units?
+        self.fc2 = nn.Linear(20, 2) #change to 2 labels
         self.leakyrelu = nn.LeakyReLU()
 
     #forward pass
     def forward(self, batch, tag, hidden):
-    	output = self.embedding(batch).view(1, BATCH_SIZE, -1) #embedding of vector, squeezes embeddings to be seq_length*embedding_dim each
-    	#output = self.leakyrelu(self.dense(output))
-    	output, hidden = self.gru(output, hidden)
-    	output = self.trans_encoder(output) #transformer encoder layer, maybe I should encode the hidden state too? nah
+    	embed = self.embedding(batch).view(1, BATCH_SIZE, -1) #embedding of vector
+    	output = embed
+    	output, hidden = self.gru(output, hidden) #GRU layer
     	output = torch.cat((output.squeeze(), tag), dim = 1) #concatinate hashtag count
     	output = self.leakyrelu(self.fc1(output)) #one fully connected layer
     	output = self.fc2(output) #second FC layer
     	return output, hidden
 
+    #initializes the hidden state for a new tensor input
+    #maybe we can learn initial states?
     def init_hidden(self, batch_size):
-        return torch.zeros(3, batch_size, self.hidden_size, device=device) #change first dimension for number of hidden layers of gru
+        return torch.zeros(1, batch_size, self.hidden_size, device=device) #change first dimension for number of hidden layers of gru
 
 #evaluates the network, similar to assignment 3
 #
-# def eval_batch(batch_tweets, batch_tag, batch_label):
-#     net.eval() 
-#     criterion = nn.CrossEntropyLoss(reduction='mean')
-#     hidden = net.init_hidden(BATCH_SIZE)
-#     net.zero_grad()
-#     for i in range(batch_tweets.size()[1]): #forward pass
-#     	output, hidden = net(batch_tweets[:,i], batch_tag, hidden)
-#     values, predicted = torch.max(output.data, 1) #index of highest energy, needs to squeeze it for dimension sake
-#     #total = batch_label.size(0)
-#     correct = (predicted == batch_label.squeeze().data).sum()
-#     #print(correct.float()/total)
-#     loss = criterion(output, batch_label.squeeze())
-#     net.train()
-#     return loss, correct.float()
 def eval_batch(batch_tweets, batch_tags, batch_labels):
 	net.eval() 
 	hidden = net.init_hidden(BATCH_SIZE) #initialize hidden state per tweet
@@ -109,18 +93,6 @@ def eval_batch(batch_tweets, batch_tags, batch_labels):
 	return loss, correct.float()
 
 #trains network
-# def train(batch_tweets, batch_tag, batch_label):
-#     hidden = net.init_hidden(BATCH_SIZE) #initialize hidden state per tweet
-#     net.zero_grad()
-#     for i in range(batch_tweets.size()[1]): #forward pass
-#     	output, hidden = net(batch_tweets[:,i], batch_tag, hidden) #word by word input, don't know any other way to train it
-#     #print(output.size())
-#     #print(batch_label.size())
-#     loss = criterion(output, batch_label.squeeze())
-#     loss.backward() #backward pass
-#     optimizer.step()
-#     return output, loss.item()
-
 def train(batch_tweets, batch_tags, batch_labels):
 	hidden = net.init_hidden(BATCH_SIZE) #initialize hidden state per tweet
 	net.zero_grad()
@@ -130,11 +102,12 @@ def train(batch_tweets, batch_tags, batch_labels):
 	optimizer.step()
 	return output, loss.item()
 
-
 if __name__ == '__main__':
 	device = "cuda" #make sure on GPU
-	BATCH_SIZE = 250
-	NUM_EPOCHS = 50
+	BATCH_SIZE = 100
+	NUM_EPOCHS = 100
+
+	writer = SummaryWriter(log_dir='./runs')
 
 	#Open data dictionary, made with clean.py
 	with open('data-dict.pickle', 'rb') as handle:
@@ -155,10 +128,8 @@ if __name__ == '__main__':
 	for tweet in tweets_test:
 		twitter_lang.add_tweet(tweet)
 
-
 	pad_index = twitter_lang.word2index['<pad>']
 
-	#lists of tensors basically
 	tweets_train = torch.stack([tensor_from_text(twitter_lang, tweet).cuda() for tweet in tweets_train], 0)
 	tweets_test = torch.stack([tensor_from_text(twitter_lang, tweet).cuda() for tweet in tweets_test], 0)
 
@@ -174,12 +145,12 @@ if __name__ == '__main__':
 	net = Net(vocab_size = twitter_lang.n_words, 
 				seq_length = seq_length,
 				embedding_size = 10,
-				hidden_size = 72,
+				hidden_size = 50,
 				padding_idx =  pad_index).cuda() #hidden size is arbitrary for now
 	criterion = nn.CrossEntropyLoss() #maybe experiment with different loss. NLLL?
 	optimizer = optim.Adam(net.parameters()) #weight decay??
 
-	
+
 	#if you want to pretrain
 	#pretrained_dict = torch.load("mytraining.pth") 
 	net.train()
@@ -227,4 +198,9 @@ if __name__ == '__main__':
 
 		print('EPOCH: %d train_loss: %.5f train_acc: %.5f test_loss: %.5f test_acc %.5f' %
               (epoch+1, train_loss, train_acc, test_loss, test_acc))
-	torch.save(net.state_dict(), 'mytraining.pth') #save state dictionary
+	#torch.save(net.state_dict(), 'mytraining.pth') #save state dictionary
+	net.eval()
+	batch_tweets, batch_hashtags, batch_labels = tweets_train[0:BATCH_SIZE], hashtags_train[0:BATCH_SIZE], labels_train[0:BATCH_SIZE]
+	hidden = net.init_hidden(BATCH_SIZE)
+	#writer.add_graph(net, input_to_model=(batch_tweets[:,0], batch_hashtags, hidden))
+	#writer.close()
